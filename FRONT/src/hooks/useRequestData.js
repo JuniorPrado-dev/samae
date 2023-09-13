0c84b06a7779142692ff31e024840efd48936e24
JuniorPrado-dev <65311268+JuniorPrado-dev@users.noreply.github.com>
2023-09-13 14:00:57 -0300

    Add files via upload

---
+++ FRONT/src/hooks/useRequestData.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+export const useRequestData = (url, headers) => {
+  const [data, setData] = useState()
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')
+  // const [up,setUp]=useState(false)
+  // console.log(update)
+  const getData=()=>{
+    setIsLoading(true)
+    axios
+      .get(url, headers)
+      .then((res) => {
+          setData(res.data)
+          setIsLoading(false)
+      }).catch((err) => {
+        setError(err);
+        setIsLoading(false)
+      })
+  }
+
+  useEffect(() => {
+    getData();
+    //setIsLoading(true)
+    
+  }, [url])
+
+  return [data, isLoading, error,getData]
+}
