1a22731ffa5b00dac4c566d239f2e53ae05ba46a
Rony <ronaldyalexandre77@example.com>
2023-09-27 16:24:48 -0300

    Upgrade do UseForm; formulário da aba aluno passado para a aba anamnese; aba aluno sendo usada para teste da nova funcionalidade do useForm: receber arquivos, majoritariamente fotos nesse teste

--- FRONT/src/components/pages/Anamnese.jsx
+++ FRONT/src/components/pages/Anamnese.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Cadastrar  from '../Form-student/form_student ';
 
 export const Anamnese = () => {
-    return <div>Anamnese</div>;
+    return( 
+        <div>
+            <Cadastrar/>
+        </div>
+    )
 };
--- FRONT/src/hooks/useForm.js
+++ FRONT/src/hooks/useForm.js
@@ -1,14 +1,28 @@
-import {useState} from "react";
+import { useState } from "react";
 
-export const  useForm = (initialState) => {
-    const [ form, setForm ] = useState(initialState)
-    const onChange = ( event ) => {
-        const {name, value} = event.target
-        const newForm = { ...form, [name]: value }
-        setForm(newForm)
+export const useForm = (initialState) => {
+    const [form, setForm] = useState(initialState)
+    const onChange = (event) => {
+        const { name, value, files } = event.target
+
+        if (files) {
+            const file = files[0]
+            if (file) {
+                const newRender = new FileReader()
+                newRender.onload = (e) => {
+                    const url = e.target.result
+                    const newForm = { ...form, picture: url }
+                    setForm(newForm)
+                }
+                newRender.readAsDataURL(file)
+            }
+        } else {
+            const newForm = { ...form, [name]: value }
+            setForm(newForm)
+        }
     }
     const resetState = () => {
         setForm(initialState)
     }
-    return [form, onChange, resetState ]
+    return [form, onChange, resetState]
 }
