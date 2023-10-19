96204ca3bcc28c1c10587ca2d27ff547b7f34073
Rony <ronaldyalexandre77@example.com>
2023-10-19 13:49:55 -0300

    debugs do ultimo commit

--- FRONT/src/components/pages/Alunos.jsx
+++ FRONT/src/components/pages/Alunos.jsx
@@ -2,39 +2,25 @@ import React from 'react';
 import { useState } from 'react';
 import { useForm } from '../../hooks/useForm';
 import Cadastrar  from '../form-student/form_student';
-import { AddTask, Adicionar, AlunoImg, CardAluno, DeleteCard, InserirAluno, MainAluno, Icon, IconX } from './styled';
+import { MainAluno, AlunoImg, CardAluno, Text , Icon, IconX, Adicionar, AddTask, DeleteCard, InserirAluno } from './styled';
 import Student from '../../img/add.png';
+import Trash from '../../img/lixeira.png';
+
 
 export const Alunos = () => {
-
+    
     const [watcher, setWatch] = useState(false)
     const [watcher2, setWatch2] = useState(true)
 
-
+    
     const [itens, setItens] = useState([])
 
-    const [form, onChange, resetState] = useForm({
-        name: ""
-    })
-
-    function addTask() {
-        setWatch(true)
+    function addTask(){
+        setWatch(true)    
         setWatch2(false)
     }
 
-    function post() {
-        const newItem = {
-            nome: form.name,
-            imagem: form.picture
-        }
-        const newList = [...itens, newItem]
-        setItens(newList)
-        resetState()
-        setWatch(false)
-        setWatch2(true)
-    }
-
-    function cancel() {
+    function cancel(){
         resetState()
         setWatch(false)
         setWatch2(true)
@@ -51,39 +37,86 @@ export const Alunos = () => {
         const file = event.target.files[0];
 
         if (file) {
-            const imageUrl = URL.createObjectURL(file);
-            setSelectedImage(imageUrl);
+        const imageUrl = URL.createObjectURL(file);
+        setSelectedImage(imageUrl);
         }
     };
 
-    return (
-        <MainAluno>
-            {itens.map((item, index) =>
-                <CardAluno key={index}>
-                    <p>{item.nome}</p>
-                    <AlunoImg src={item.imagem} />
-                    <DeleteCard type='button' onClick={() => deleteTask(item)}>
-                        <IconX src="../../../img/lixeira.png" />
-                    </DeleteCard>
-                </CardAluno>)}
-            {watcher &&
-                <InserirAluno>
-                    <Cadastrar/>
-                    <input type="text" id="name" name="name" onChange={onChange} value={form.name} />
-                    <input type="file" accept="image/jpeg, image/png" id="picture" name="picture" onChange={onChange} />
-                    <button type="submit" onClick={post}>Enviar</button>
-                    <button type="button" onClick={cancel}> X </button>
-                </InserirAluno>
-            }
+    const [form, onChange, resetState] = useForm({ 
+        name: '',
+        date: '', 
+        parent: '', 
+        contact: '' 
+    })
 
-            {watcher2 &&
-                <Adicionar>
-                    <AddTask onClick={addTask} type='button'>
+    const [gender, setGender] = useState('')
+  
+    const mudaGender = (e) => {
+      setGender(e.target.value)
+    }
+  
+    function send(e) {
+      e.preventDefault()
+      const aluno = {
+        name: form.name,
+        date: form.date,
+        gender: gender,
+        parent: form.parent,
+        contact: form.contact,
+        imagem: form.picture
+      }
+
+        const newItem ={
+            nome: form.name,
+            imagem:form.picture
+        }
+        const newList = [...itens,newItem ]
+        setItens(newList)
+        resetState()
+        setWatch(false)
+        setWatch2(true)
+
+        axios.post('http://localhost:3003/sign-up-student', aluno)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
+return (
+    <main>
+        <div>
+            <MainAluno>  
+                {itens.map( (item, index) =>
+                    <CardAluno key = { index }>
+                        <p>{item.nome}</p>
+                        <AlunoImg src={item.imagem} />
+                        <DeleteCard type='button' onClick={()=>deleteTask(item)}>
+                        <IconX src={Trash} />
+                        </DeleteCard>
+                    </CardAluno>) }
+                {watcher && 
+                    <InserirAluno>
+                        <Cadastrar
+                            form={form}
+                            onChange = {onChange}
+                            mudaGender = {mudaGender}
+                            cancel={cancel}
+                            send = {send}
+                        />
+                    </InserirAluno>
+                }
+            </MainAluno>
+                {watcher2 &&
+                    <Adicionar>
+                        <AddTask onClick={addTask} type='button'>
                         <Icon src={Student} />
-                    </AddTask>
-                </Adicionar>
-            }
-        </MainAluno>
-
+                        </AddTask>
+                    </Adicionar>
+                }
+        </div>
+    </main>
     )
 };
